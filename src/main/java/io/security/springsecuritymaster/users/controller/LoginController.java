b840d00f3ecdc93b56502f1e91cedc76bacdc880
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-06-13 15:39:04 +0900

    feat: customize logout

--- src/main/java/io/security/springsecuritymaster/users/controller/LoginController.java
+++ src/main/java/io/security/springsecuritymaster/users/controller/LoginController.java
@@ -1,5 +1,10 @@
 package io.security.springsecuritymaster.users.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -14,5 +19,19 @@ public class LoginController {
     @GetMapping("/signup")
     public String signup() {
         return "login/signup";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication();
+
+        // 인증된 상태에서 로그아웃 처리
+        if (authentication != null) {
+            // 여러 LogoutHandler 구현체들 중에서 Session까지 무효화시키는 SecurityContextLogoutHandler의 logout()을 사용
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+
+        // 로그아웃 시 로그인 페이지로 redirect
+        return "redirect:/login";
     }
 }
